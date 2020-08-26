7ec174fc69d8e47111e7c45d1f6cddbf3721dbe6
Kenneth Rose <kennethrose82@gmail.com>
2020-08-26 22:48:43 +0200

    parse the folder structure in a new way.

--- R/entsoe_list_files.R
+++ R/entsoe_list_files.R
@@ -12,14 +12,32 @@ entsoe_list_files <- function(basis_name){
   # con <- httr::content(x = req, as = "text", encoding = "UTF-8")
   con <- rawToChar(req$content)
 
-  con_df <- parse_files(con)
-  con_df <- lapply(con_df, as.data.frame, stringsAsFactors = FALSE)
-  con_df <- do.call(rbind, con_df)
-  # con_df$date <- strptime(con_df$date, format = "%b %d %H:%M", tz = "UTC")
-  con_df <- dplyr::arrange(.data = con_df, file)
-  con_df <- dplyr::filter(.data = con_df, !stringr::str_detect(perm, "^d"))
+  con_df <- parse_ftp_files(con)
+
+  # con_df <- parse_files(con)
+  # con_df <- lapply(con_df, as.data.frame, stringsAsFactors = FALSE)
+  # con_df <- do.call(rbind, con_df)
+  # # con_df$date <- strptime(con_df$date, format = "%b %d %H:%M", tz = "UTC")
+  # con_df <- dplyr::arrange(.data = con_df, file)
+  # con_df <- dplyr::filter(.data = con_df, !stringr::str_detect(perm, "^d"))
 
   con_df
+}
+
+parse_ftp_files <- function(x){
+  df_files <- as.data.frame(suppressWarnings(readr::read_fwf(file = x, readr::fwf_widths(widths = c(10, 5, 4, 10, 14, 13, 60)))))
+
+  names(df_files) <- c("perm", "unknown1", "unknown2", "unknown3", "size", "date", "file")
+  time_or_year <- stringr::str_sub(df_files$date, start = -5)
+  time_and_year <- dplyr::if_else(!stringr::str_detect(time_or_year, " [0-9]{4}"), paste0(lubridate::year(Sys.Date()), " ", time_or_year), paste0(time_or_year, " 00:00"))
+  df_files$date <- stringr::str_replace(df_files$date, time_or_year, "")
+  df_files$date <- paste0(df_files$date, time_and_year)
+  df_files$date <- stringr::str_replace(df_files$date, "  ", " ")
+  df_files$date <- lubridate::mdy_hm(df_files$date, tz = "UTC")
+
+  df_files <- df_files[df_files$unknown1 == 1, ]
+
+  df_files
 }
 
 
