2e62d49f287363f5c0c0d91917c897bd7b49577d
Kenneth Rose <kennethrose82@gmail.com>
2020-08-26 22:49:08 +0200

    Parse the folders in a new way.

--- R/entsoe_list_folders.R
+++ R/entsoe_list_folders.R
@@ -10,13 +10,30 @@ entsoe_list_folders <- function(){
   # con <- httr::content(x = req, as = "text", encoding = "UTF-8")
   con <- rawToChar(req$content)
 
-  con_df <- parse_links(con)
-  con_df <- lapply(con_df, as.data.frame, stringsAsFactors = FALSE)
-  con_df <- do.call(rbind, con_df)
-  # con_df$date <- strptime(con_df$date, format = "%b %d %H:%M", tz = "UTC")
-  con_df <- dplyr::arrange(.data = con_df, links)
+  con_df <- parse_ftp_links(con)
+  # con_df <- lapply(con_df, as.data.frame, stringsAsFactors = FALSE)
+  # con_df <- do.call(rbind, con_df)
+  # # con_df$date <- strptime(con_df$date, format = "%b %d %H:%M", tz = "UTC")
+  # con_df <- dplyr::arrange(.data = con_df, links)
 
   con_df
+}
+
+
+parse_ftp_links <- function(x){
+  df_links <- as.data.frame(readr::read_fwf(file = x, readr::fwf_widths(widths = c(10, 5, 4, 10, 14, 13, 60))))
+
+  names(df_links) <- c("perm", "unknown1", "unknown2", "unknown3", "size", "date", "link")
+  time_or_year <- stringr::str_sub(df_links$date, start = -5)
+  time_and_year <- dplyr::if_else(!stringr::str_detect(time_or_year, " [0-9]{4}"), paste0(lubridate::year(Sys.Date()), " ", time_or_year), paste0(time_or_year, " 00:00"))
+  df_links$date <- stringr::str_replace(df_links$date, time_or_year, "")
+  df_links$date <- paste0(df_links$date, time_and_year)
+  df_links$date <- stringr::str_replace(df_links$date, "  ", " ")
+  df_links$date <- lubridate::mdy_hm(df_links$date, tz = "UTC")
+
+  df_links <- df_links[df_links$unknown1 == 2, ]
+
+  df_links
 }
 
 
